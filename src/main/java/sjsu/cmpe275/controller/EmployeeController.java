4f0f4cf1196d553994a1f7d58d107c893825cf07
Prit-Lakhani <priiitttt@gmail.com>
2023-04-18 13:14:17 -0700

    javaDoc comments added

--- src/main/java/sjsu/cmpe275/controller/EmployeeController.java
+++ src/main/java/sjsu/cmpe275/controller/EmployeeController.java
@@ -18,7 +18,24 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    // Implement the required REST API methods, e.g., createEmployee, getEmployee, etc.
+
+    /**
+     * Creates a new employee for the specified employer and returns a ResponseEntity with the created employee in JSON or XML format,
+     * depending on the value of the "format" parameter. If the creation of the employee fails, a bad request response is returned.
+     *
+     * @param name       the name of the employee
+     * @param email      the email address of the employee (optional)
+     * @param title      the job title of the employee (optional)
+     * @param street     the street address of the employee (optional)
+     * @param city       the city of the employee (optional)
+     * @param state      the state of the employee (optional)
+     * @param zip        the zip code of the employee (optional)
+     * @param managerId  the ID of the employee's manager (optional)
+     * @param employerId the ID of the employer to whom the employee belongs
+     * @param format     the format of the response (either "json" or "xml")
+     * @return a ResponseEntity with the created employee in JSON or XML format, depending on the value of the "format" parameter
+     * @throws ResponseStatusException if there is an error while creating the employee
+     */
     @PostMapping(value = "/create/{employerId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Employee> createEmployee(
             @RequestParam("name") String name,
@@ -34,7 +51,7 @@ public class EmployeeController {
 
         Employee newEmployee = employeeService.createEmployee(name, email, title, street, city, state, zip, managerId, employerId);
 
-            if (newEmployee == null) {
+        if (newEmployee == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
 
@@ -49,6 +66,15 @@ public class EmployeeController {
         return ResponseEntity.status(HttpStatus.OK).body(newEmployee);
     }
 
+    /**
+     * Exception handler method that handles instances of ResponseStatusException.
+     * It returns a ResponseEntity object that includes the status code and the reason
+     * for the exception as a String.
+     *
+     * @param ex the ResponseStatusException that needs to be handled
+     * @return a ResponseEntity object that includes the status code and the reason
+     * for the exception as a String
+     */
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
         return ResponseEntity
--- src/main/java/sjsu/cmpe275/service/EmployeeService.java
+++ src/main/java/sjsu/cmpe275/service/EmployeeService.java
@@ -28,6 +28,12 @@ public class EmployeeService {
     @Autowired
     private EntityManager entityManager;
 
+    /**
+     * Generates a new employee ID for a specified employer.
+     *
+     * @param employerId the ID of the employer for whom the new employee ID will be generated
+     * @return a new employee ID that is unique within the specified employer's employee list
+     */
     public long generateEmployeeId(long employerId) {
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Long> cq = cb.createQuery(Long.class);
@@ -37,6 +43,22 @@ public class EmployeeService {
         return maxId == null ? 1 : maxId + 1;
     }
 
+
+    /**
+     * Creates a new Employee with the given name, email, title, street, city, state, zip, managerId, and employerId.
+     *
+     * @param name       the name of the Employee
+     * @param email      the email address of the Employee
+     * @param title      the job title of the Employee
+     * @param street     the street address of the Employee
+     * @param city       the city of the Employee's address
+     * @param state      the state of the Employee's address
+     * @param zip        the zip code of the Employee's address
+     * @param managerId  the ID of the Employee's manager (nullable)
+     * @param employerId the ID of the Employer associated with the Employee
+     * @return the newly created Employee
+     * @throws ResponseStatusException if the Employer object does not exist or the Manager does not exist
+     */
     public Employee createEmployee(String name, String email, String title, String street, String city, String state, String zip, Long managerId, long employerId) throws ResponseStatusException {
         Employer optionalEmployer = employerRepository.findById(employerId);
         if (optionalEmployer == null) {
@@ -44,9 +66,9 @@ public class EmployeeService {
 
         }
         Employee Manager = null;
-        if(managerId != null) {
+        if (managerId != null) {
             Manager = employeeRepository.findByIdAndEmployerId(managerId, employerId);
-            if(Manager == null) {
+            if (Manager == null) {
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Manager does not exist!");
             }
         }
@@ -56,12 +78,12 @@ public class EmployeeService {
         long id = generateEmployeeId(employerId);
         Employee employee = new Employee(id, employerId, name, email, title, address, optionalEmployer, Manager, collaborators, reports);
 
-        if(Manager != null) {
+        if (Manager != null) {
             employee.setManager(Manager);
         }
 
         List<Employee> currentEmpList = optionalEmployer.getEmployees();
-        if(currentEmpList == null) {
+        if (currentEmpList == null) {
             currentEmpList = new ArrayList<Employee>();
         }
         currentEmpList.add(employee);
@@ -72,6 +94,13 @@ public class EmployeeService {
     }
 
 
+    /**
+     * Retrieves an Employee object from the employee repository based on the provided EmployeeId object.
+     *
+     * @param employeeId the EmployeeId object containing the ID of the employee and the ID of their employer
+     * @return the Employee object if found in the repository
+     * @throws ResponseStatusException if an Employee object with the given IDs is not found in the repository
+     */
     public Employee getEmployee(EmployeeId employeeId) throws ResponseStatusException {
         Employee optionalEmployee = employeeRepository.findByIdAndEmployerId(employeeId.getId(), employeeId.getEmployerId());
         if (optionalEmployee == null) {
@@ -80,6 +109,21 @@ public class EmployeeService {
         return optionalEmployee;
     }
 
+    /**
+     * Updates an existing employee with the provided details. If any of the fields are not provided, they will not be updated.
+     *
+     * @param employeeId the unique identifier of the employee to be updated, including their employer ID
+     * @param name       the updated name of the employee, or null if the name should not be updated
+     * @param email      the updated email of the employee, or null if the email should not be updated
+     * @param title      the updated job title of the employee, or null if the job title should not be updated
+     * @param street     the updated street address of the employee, or null if the address should not be updated
+     * @param city       the updated city of the employee, or null if the address should not be updated
+     * @param state      the updated state of the employee, or null if the address should not be updated
+     * @param zip        the updated zip code of the employee, or null if the address should not be updated
+     * @param managerId  the unique identifier of the new manager of the employee, or null if the employee should not have a manager
+     * @return the updated employee object, with any fields that were updated
+     * @throws ResponseStatusException if the employee or manager is not found, or if the manager is not from the same employer, or if the employee tries to set themselves as their own manager
+     */
     public Employee updateEmployee(EmployeeId employeeId, String name, String email, String title, String street, String city, String state, String zip, Long managerId) throws ResponseStatusException {
         Employee optionalEmployee = employeeRepository.findByIdAndEmployerId(employeeId.getId(), employeeId.getEmployerId());
         if (optionalEmployee == null) {
@@ -145,10 +189,19 @@ public class EmployeeService {
         return savedEmployee;
     }
 
+    /**
+     * Deletes an Employee with the given ID and employer ID from the database, if found, and returns the deleted Employee object.
+     *
+     * @param id         The ID of the Employee to be deleted.
+     * @param employerId The ID of the employer of the Employee to be deleted.
+     * @return The deleted Employee object.
+     * @throws ResponseStatusException If the Employee with the given ID and employer ID is not found, or if the Employee has reports and cannot be deleted.
+     */
     @Transactional
     public Employee deleteEmployee(Long id, Long employerId) throws ResponseStatusException {
         Employee optionalEmployee = employeeRepository.findByIdAndEmployerId(id, employerId);
-        if (optionalEmployee != null) {;
+        if (optionalEmployee != null) {
+            ;
             if (!optionalEmployee.getReports().isEmpty()) {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Employee has reports and cannot be deleted.");
             }
